11fc49e9963ae1abe17412605753e518d97ce812
COREY COSMAN <cosman.corey@gmail.com>
2017-11-07 15:33:31 -0500

    added different response types to books app- html, text, csv, json

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,21 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
 
+    respond_to do |format|
+      format.html
+      format.text
+      format.csv { render plain: Book.generate_csv(@books)}
+      format.json do
+        json_data = @books.map { |book|
+
+        {title: book.title,
+         author: book.author,
+         already_read: book.already_read}
+        }
+        render json: json_data
+      end
+    end
+  end
 end
